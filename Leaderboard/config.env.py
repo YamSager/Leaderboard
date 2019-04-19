db55d0ea07fe0cc84968ad61d18a5c93d1fed36e
yamsager <sly4758@rit.edu>
2019-04-19 13:06:42 -0400

    Updated Configeration

--- Leaderboard/config.env.py
+++ Leaderboard/config.env.py
@@ -4,7 +4,6 @@ import os
 DEBUG=False
 IP=os.environ.get('IP', '0.0.0.0')
 PORT=os.environ.get('PORT', '8080')
-SERVER_NAME = os.environ.get('LEADERBOARD_NAME', 'leaderboard.csh.rit.edu:443')
 
 BIND_DN=os.environ["BIND_DN"]
 BIND_PW=os.environ["BIND_PW"]
