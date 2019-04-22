957e57ecca6217780e991ad00df8e4851baf0ff3
yamsager <sly4758@rit.edu>
2019-04-22 13:15:43 -0400

    Updated api to insert foosball scores

--- Leaderboard/__init__.py
+++ Leaderboard/__init__.py
@@ -24,10 +24,10 @@ def foosballGamePost():
         score1 = int(reqData["score1"])
         score2 = int(reqData["score2"])
         if player1 != player2 and player1 is not None and player2 is not None and (score1 == 10 or score2 == 10):
-            #conn = psycopg2.connect(host="postgres.csh.rit.edu",database="leaderboard",user=app.config["PSQL_USER"],password=app.config["PSQL_PW"],options="-c search_path=public")                
-            #c = conn.cursor()
-            #c.execute('INSERT INTO foosballGame (player1, player2, score1, score2) VALUES ("' + player1 + '", "' + player2 + '", ' + str(score1) + ', ' + str(score2) + ')')
-            #conn.commit()
+            conn = psycopg2.connect(host="postgres.csh.rit.edu",database="leaderboard",user=app.config["PSQL_USER"],password=app.config["PSQL_PW"],options="-c search_path=public")                
+            c = conn.cursor()
+            c.execute('INSERT INTO foosballGame (player1, player2, score1, score2) VALUES ("' + player1 + '", "' + player2 + '", ' + str(score1) + ', ' + str(score2) + ')')
+            conn.commit()
             print('INSERT INTO foosballGame (player1, player2, score1, score2) VALUES ("' + player1 + '", "' + player2 + '", ' + str(score1) + ', ' + str(score2) + ')')
             return "200"
         else:
